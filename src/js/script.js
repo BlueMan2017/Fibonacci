7682f88b7d4cab93d49494633ed0bebaad328278
BlueMan2017 <s.k.tomaszewska@gmail.com>
2019-04-28 16:58:27 +0200

    zmiany

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,14 @@
-function fibonacci(i) {
+const numbers = num => {
   let arr = [];
-  let fib = n => {
-    if (n < 2) {
-      return n;
-    }
-    return fib(n - 1) + fib(n - 2);
-  };
-  let num = fib(i);
+
   for (let i = 0; i < num; i++) {
-    arr.push(1);
+    let fibo = [0, 1];
+    for (let i = 2; i < num + 1; i++) {
+      fibo.push(fibo[i - 2] + fibo[i - 1]);
+    }
+    arr.push(...fibo);
   }
-  return arr.length;
-}
+  return arr[num];
+};
 
-console.log(fibonacci(1));
-
-function fibo(n) {
-  let arr = [0, 1];
-  for (let i = 2; i < n + 1; i++) {
-    arr.push(arr[i - 2] + arr[i - 1]);
-  }
-  return arr[n];
-}
-
-console.log(fibo(1));
+console.log(numbers(7));
